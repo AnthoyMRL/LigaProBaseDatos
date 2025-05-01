f7feb40953bb18843f4ef0ecf4b04d6ae3ced696
AnthonyMRL <am4866692@gmail.com>
2025-04-30 23:56:23 -0500

    Agregar archivos de proyecto.

---
+++ LigaProBaseDatos/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LigaProBaseDatos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
